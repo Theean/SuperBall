85398c265542a53a26fd63cb4213015542b2f4d9
Philip <1016942@student.sae.edu.au>
2021-01-27 02:49:03 +1100

    ScoreScript
    basic Score counter (NO UI yet)

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public int currentScore;
+    public int scorePerNode = 100;
+    public int perfectMod = 10;
+    public int greatMod = 5;
+    public int goodMod = 2;
+
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
+
+    public int currentCombo;
+    public int maxCombo;
+    public int comboBase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMultiplier = 1;
+        comboBase = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NodeHit()
+    {
+        currentCombo++;
+
+
+        
+        if(currentCombo > maxCombo)
+        {
+            maxCombo = currentCombo;
+        }
+
+        if (currentMultiplier - 1 < multiplierThresholds.Length)
+        {
+            multiplierTracker++;
+
+            if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+
+            }
+        }
+
+        currentScore += scorePerNode * currentMultiplier;
+        currentScore += currentCombo * comboBase * currentMultiplier;
+    }
+
+    public void NodeMiss()
+    {
+        Debug.Log("Missed Node");
+
+        currentMultiplier = 1;
+        currentCombo = 0;
+        multiplierTracker = 0;
+    }
+
+
+}
